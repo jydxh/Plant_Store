096ee65d00b6839db01e3a44cdf5a845520792db
jydxh <wzxxds@gmail.com>
2025-07-10 15:51:13 -0400

    feat: finish the  fetching product detail logic todo: render the data at dom

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Property } from 'src/app/services/product.module';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit, OnDestroy {
+  product: Property | undefined = undefined;
+  isLoading = false;
+  // if any error occurred at fetching data, the page will redirect to 404 page
 
+  private subscription: Subscription = new Subscription();
+  constructor(
+    public productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      /* part C : use route params to fetch the product and display details */
+      this.route.params.subscribe((params) => {
+        this.loadProductDetail(params['id']);
+      })
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  loadProductDetail(id: string) {
+    this.isLoading = true;
+
+    this.productService.fetchProductById(id).subscribe({
+      next: (data) => {
+        console.log('product: ', data);
+        this.product = data.data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        /* this is for the partC: if no product is found for the ID, redirect to the page not found */
+        console.log(error);
+        this.isLoading = false;
+        this.router.navigate(['/notFound']);
+      },
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, Observable, throwError } from 'rxjs';
-import { FetchedProductsResponse } from './product.module';
+import { FetchedProductsResponse, Property } from './product.module';
 
 @Injectable({
   providedIn: 'root',
@@ -21,7 +21,11 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   // fetch products logic
-
+  /* note: part C is asking create a hardcoded list of products in product service,
+  here I am fetching the productLists from my own backend, and will consume this list
+  at home page, and each product at home page will push the URL to /products/:id route,
+  if no product found from backend, it will redirect to 404 page 
+   */
   fetchProducts(page: number = 1): Observable<FetchedProductsResponse> {
     const params = new HttpParams().set('page', page.toString());
     return this.http
@@ -30,6 +34,20 @@ export class ProductService {
         catchError((error: HttpErrorResponse) => {
           console.log(error);
           return throwError(() => new Error('failed in fetch products'));
+        })
+      );
+  }
+
+  /* fetch the product detail by productId */
+  fetchProductById(id: string): Observable<{ data: Property }> {
+    return this.http
+      .get<{ data: Property }>(
+        this.API_URL + '/products/detail?productId=' + id
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          console.log(error);
+          return throwError(() => new Error('failed in fetch products: ' + id));
         })
       );
   }
